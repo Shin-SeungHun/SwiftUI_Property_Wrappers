b86d54c37af1ea33b28a7181294fb05db3b0c52a
shin-SeungHun <sng7387@gmail.com>
2024-03-26 16:36:25 +0900

    refactor: model과 mock data list 작성

--- SimplePost/ContentView.swift
+++ SimplePost/ContentView.swift
@@ -19,6 +19,23 @@ struct ContentView: View {
     }
 }
 
+struct Post: Identifiable {
+    let id = UUID()
+    let username: String
+    let content: String
+}
+
+extension Post {
+    static var list: [Post] = [
+        Post(username: "1", content: "1"),
+        Post(username: "2", content: "2"),
+        Post(username: "3", content: "3"),
+        Post(username: "4", content: "4"),
+        Post(username: "5", content: "5"),
+        Post(username: "6", content: "6"),
+    ]
+}
+
 #Preview {
     ContentView()
 }
