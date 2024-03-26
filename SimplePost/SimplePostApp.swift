44d007a1ee42aa1b4a85e285983948a727f269fe
shin-SeungHun <sng7387@gmail.com>
2024-03-26 15:50:22 +0900

    Initial Commit

---
+++ SimplePost/SimplePostApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePostApp.swift
+//  SimplePost
+//
+//  Created by 신승훈 on 2024/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SimplePostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
