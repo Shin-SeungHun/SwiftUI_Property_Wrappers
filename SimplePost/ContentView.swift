baff5cd0f9e4b41ad71f032d15d97088cae362b3
shin-SeungHun <sng7387@gmail.com>
2024-03-26 16:39:24 +0900

    refactor: 각각의 row가 될 item View 구성

--- SimplePost/ContentView.swift
+++ SimplePost/ContentView.swift
@@ -19,12 +19,40 @@ struct ContentView: View {
     }
 }
 
+struct PostRow: View {
+    let post: Post
+    let colors: [Color] = [
+        Color.orange, Color.green, Color.purple, Color.pink, Color.blue, Color.yellow, Color.brown, Color.cyan, Color.mint, Color.indigo, Color.teal
+    ]
+    var body: some View {
+        HStack {
+            Circle()
+                .fill(colors.randomElement() ?? .brown)
+                .frame(width: 30)
+            VStack(alignment: .leading) {
+                Text(post.username)
+                Text(post.content)
+                    .font(.title)
+            }
+            Spacer()
+        }
+        .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 10)
+                .strokeBorder()
+        }
+        .padding()
+    }
+}
+
+/// model
 struct Post: Identifiable {
     let id = UUID()
     let username: String
     let content: String
 }
 
+// mock
 extension Post {
     static var list: [Post] = [
         Post(username: "1", content: "1"),
@@ -37,5 +65,6 @@ extension Post {
 }
 
 #Preview {
-    ContentView()
+//    ContentView()
+      PostRow(post: Post(username: "스티브", content: "안녕하세요"))
 }
